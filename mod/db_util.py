a7dc4ed55fea9d16efac20ca1103f4eb6e649211
zhanghaokang <zhanghaokang@antiy.cn>
2020-06-29 18:07:49 +0800

    Bug | fix mysql connect bug

--- mod/db_util.py
+++ mod/db_util.py
@@ -4,27 +4,27 @@
 @Author: Rodney Cheung
 @Date: 2020-06-29 11:02:22
 @LastEditors: Rodney Cheung
-@LastEditTime: 2020-06-29 16:35:59
+@LastEditTime: 2020-06-30 09:58:10
 @FilePath: /python-mod/mod/db_util.py
 '''
 import sqlite3
-import mysql.connector
-from mysql.connector import errorcode
-import sshtunnel
+from sshtunnel import SSHTunnelForwarder
+import pymysql
 
 
 class DataBase(object):
     def __init__(self):
         self.connection = None
-        self.cursor = None
 
     def connect(self):
         pass
 
     def close(self):
-        self.cursor.close()
         self.connection.commit()
         self.connection.close()
+
+    def __del__(self):
+        self.close()
 
 
 class Sqlite3DataBase(DataBase):
@@ -35,7 +35,6 @@ class Sqlite3DataBase(DataBase):
     def connect(self):
         self.connection = sqlite3.connect(self.db_path,
                                           check_same_thread=False)
-        self.cursor = self.connection.cursor()
 
 
 class MySqlDataBase(DataBase):
@@ -56,55 +55,27 @@ class MySqlDataBase(DataBase):
         self.tunnel = None
 
     def connect(self):
-        try:
-            config = {
-                'user': self.username,
-                'password': self.password,
-                'host': self.host,
-                'port': self.port,
-                'database': self.database_name,
-                'raise_on_warnings': self.raise_on_warnings
-            }
-            self.connection = mysql.connector.connect(config)
-            self.cursor = self.connection.cursor
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
+        self.connection = pymysql.connect(host=self.host,
+                                          port=self.port,
+                                          user=self.username,
+                                          password=self.password,
+                                          db=self.database_name)
 
     def connect_by_ssh_tunnel(self, ssh_host, ssh_port, ssh_username,
                               ssh_private_key):
-        try:
-            with open(ssh_private_key) as f:
-                pkey = f.read()
-                self.tunnel = sshtunnel.SSHTunnelForwarder(
-                    (ssh_host, ssh_port),
-                    ssh_username=ssh_username,
-                    ssh_pkey=pkey,
-                    remote_bind_address=(self.host, self.port))
-                self.tunnel.start()
-                config = {
-                    'user': self.username,
-                    'password': self.password,
-                    'host': '127.0.0.1',
-                    'port': self.tunnel.local_bind_port,
-                    'database': self.database_name,
-                    'raise_on_warnings': self.raise_on_warnings
-                }
-                self.connection = mysql.connector.connect(config)
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
+        self.tunnel = SSHTunnelForwarder(
+            (ssh_host, ssh_port),
+            ssh_username=ssh_username,
+            ssh_pkey=ssh_private_key,
+            remote_bind_address=(self.host, self.port))
+        self.tunnel.start()
+        self.connection = pymysql.connect(host='127.0.0.1',
+                                          port=self.tunnel.local_bind_port,
+                                          user=self.username,
+                                          password=self.password,
+                                          db=self.database_name)
 
     def close(self):
-        self.cursor.close()
         self.connection.commit()
         self.connection.close()
         if self.tunnel is not None:
