b3178f516b170b62d6f1874b6a7e356a8f1b63eb
zhanghaokang <zhanghaokang@antiy.cn>
2020-08-12 17:04:34 +0800

    Develop | optimize time util code && add time_util unittest

--- src/mod/time_util.py
+++ src/mod/time_util.py
@@ -8,22 +8,29 @@
 @FilePath: /python-mod/core/time_util.py
 """
 
-import datetime
 import time
 
 
-def format_time_use(time):
-    m, s = divmod(time, 60)
+def format_seconds(seconds, time_format):
+    """
+    convert seconds to specified format
+    Args:
+        seconds: seconds
+        time_format: specified time format,hour minute & second
+        can be used
+
+    Returns:
+        formatted time str
+    """
+    m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
-    if h > 0:
-        return "Cost: %dh%02dm%02ds" % (h, m, s)
-    elif m > 0:
-        return "Cost: %02dm%02ds" % (m, s)
-    else:
-        return "Cost: %02ds" % s
+    return str(time_format).format(h, m, s)
 
 
 def get_current_timestamp():
-    return time.mktime(
-        time.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                      "%Y-%m-%d %H:%M:%S"))
+    """
+    get current timestamp
+    Returns:
+        current timestamp
+    """
+    return time.time()
---
+++ test/test_time_util.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+# -*-coding:utf-8 -*-
+
+"""
+# File       : test_time_util.py
+# Time       ：2020/8/12 16:32
+# Author     ：Rodney Cheung
+"""
+import unittest
+from mod import time_util
+
+
+class TestTimeUtil(unittest.TestCase):
+    def test_format_time_use(self):
+        print(time_util.format_seconds(100, '{:d}h{:d}m{:d}s'))
+
+    def test_get_current_timestamp(self):
+        print(time_util.get_current_timestamp())
+
+
+if __name__ == '__main__':
+    unittest.main()
