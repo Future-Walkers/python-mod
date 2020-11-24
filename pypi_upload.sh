465c0784124fa938615b4fd99e0fc95f71ddee14
zhanghaokang <zhanghaokang@antiy.cn>
2020-11-24 17:52:10 +0800

    Develop | upgrade pypi_upload.sh

--- pypi_upload.sh
+++ pypi_upload.sh
@@ -1,14 +1,5 @@
-#!/bin/zsh
+#!/bin/sh
 
-if [ -d build ]; then
-  echo "$1" | sudo -S rm -dr build
-fi
-if [ -d dist ]; then
-  sudo rm -dr dist
-fi
-if [ -d src/wisbec.egg-info ]; then
-  sudo rm -dr src/wisbec.egg-info
-fi
-
-echo "$1" | sudo -S python3 setup.py sdist bdist_wheel
+source clean_build.sh
+python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
